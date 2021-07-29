93d405edf27b1ca57155345826c2b184a881bdb9
miker83z <mikerz83@gmail.com>
2021-07-29 17:25:36 +0200

    finished test

--- statistics.py
+++ statistics.py
@@ -11,32 +11,42 @@ tmpGasExt = []
 
 def plot1():
     fig, _ = plt.subplots(nrows=1, ncols=1, constrained_layout=True)
-    fig.set_size_inches(10, 7.5)
-    red_patch = mpatches.Patch(color='tab:red', label='Reduction method')
+    fig.set_size_inches(7, 5)
     blue_patch = mpatches.Patch(
-        color='tab:blue', label='Pref Extensions method')
+        color='tab:blue', label='Enumerating Preferred Extensions of AF method')
+    red_patch = mpatches.Patch(
+        color='tab:red', label='Reductions of PAF to AF (PR3) method')
 
     sxrange = []
     width = 0.4
+    j = -1
     for i in range(len(gasMeasurementsRed)):
-        if i % 2:
-            sxrange.append(i + width - .15)
+        if i % 3 == 0:
+            j += 1.2
+            sxrange.append(i - j + .9 - width)
+        elif i % 3 == 1:
+            sxrange.append(i - j)
         else:
-            sxrange.append(i + 1.15 - width)
-    sxrange = np.array(sxrange)
-    print(sxrange)
+            sxrange.append(i - j - .9 + width)
 
-    plt.ylabel('Gas Cost', fontsize=13)
+    sxrange = np.array(sxrange)
+    # print(sxrange)
+
+    plt.ylabel('Gas Cost', fontsize=12)
     # plt.title('Argumentation Gas Cost', fontdict={'fontsize': 16}, weight='heavy')
     plt.bar(sxrange - (width/4), gasMeasurementsRed, (width/2), color='tab:red',
             align='center')
     plt.bar(sxrange + (width/4), gasMeasurementsExt, (width/2), color='tab:blue',
             align='center')
-    plt.xticks(sxrange, ['    5\np = 0.33', 'nodes        \np = 0.5', '    10\np = 0.33', 'nodes        \np = 0.5',
-                         '    15\np = 0.33', 'nodes        \np = 0.5', '    20\np = 0.33', 'nodes        \np = 0.5'])
+    plt.xticks(sxrange, ['0.33\n', '0.5\n(5 arguments)', '0.66\n',
+                         '0.33\n', '0.5\n(10 arguments)', '0.66\n',
+                         '0.33\n', '0.5\n(15 arguments)', '0.66\n',
+                         '0.33\n', '0.5\n(20 arguments)', '0.66\n'])
+    plt.xlabel('Attack Formation Probability (p)', fontsize=12)
 
     plt.legend(handles=[blue_patch, red_patch], fontsize='large')
-    plt.show()
+    # plt.show()
+    plt.savefig('./gas-cost.png', bbox_inches='tight', dpi=300)
 
 
 with open('data.csv', 'r') as csvFile:
--- test/test.js
+++ test/test.js
@@ -3,6 +3,26 @@ const Negotiation = artifacts.require('Negotiation');
 const fs = require('fs');
 const filepath = './data.csv';
 
+const printGraph = (g) => {
+  console.log('--------Graph--------');
+  for (const node of g.nodes) {
+    console.log('Node:', node.toString());
+  }
+
+  for (let i = 0; i < g.edgesSource.length; i++) {
+    console.log(
+      g.edgesSource[i].toString(),
+      ' -> ',
+      g.edgesTarget[i].toString()
+    );
+  }
+};
+
+const getRandomIntInclusive = (min, max) => {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1) + min); //The maximum is inclusive and the minimum is inclusive
+};
 /*
 contract('Argumentation 0', (accounts) => {
   const alpha = accounts[0];
@@ -45,12 +65,13 @@ contract('Argumentation 0', (accounts) => {
     //printGraph(r3);
 
     const r4 = await sc.enumeratingPreferredExtensions(3);
-    /*r4.logs.forEach((element) => {
-      console.log('***************************************');
-      console.log(element.args.args);
-    });*/
-/*  });
+    //r4.logs.forEach((element) => {
+    //  console.log('***************************************');
+    //  console.log(element.args.args);
+    //});
+  });
 });
+*/
 
 contract('Argumentation 1', (accounts) => {
   const alpha = accounts[0];
@@ -63,46 +84,86 @@ contract('Argumentation 1', (accounts) => {
     const resAlpha = await sc.insertArgument('b', {
       from: alpha,
     });
+    const resAlphaGasUsed = resAlpha.receipt.gasUsed;
     const resBeta = await sc.insertArgument('c', {
       from: beta,
     });
+    const resBetaGasUsed = resBeta.receipt.gasUsed;
     const resGamma = await sc.insertArgument('d', {
       from: gamma,
     });
+    const resGammaGasUsed = resGamma.receipt.gasUsed;
     const resAlpha2 = await sc.insertArgument('e', {
       from: alpha,
     });
+    const resAlpha2GasUsed = resAlpha2.receipt.gasUsed;
+    console.log(
+      'insertArgument(): ',
+      (resAlphaGasUsed + resBetaGasUsed + resGammaGasUsed + resAlpha2GasUsed) /
+        4
+    );
+
     const resBetaSupport = await sc.supportArgument(3, {
       from: beta,
     });
+    const resBetaSupportGasUsed = resBetaSupport.receipt.gasUsed;
     const resGammaSupport = await sc.supportArgument(2, {
       from: gamma,
     });
+    const resGammaSupportGasUsed = resGammaSupport.receipt.gasUsed;
+    console.log(
+      'supportArgument(): ',
+      (resBetaSupportGasUsed + resGammaSupportGasUsed) / 2
+    );
 
     const edgeBC = await sc.insertAttack(1, 2, '');
+    const edgeBCGasUsed = edgeBC.receipt.gasUsed;
     const edgeCD = await sc.insertAttack(2, 3, '');
+    const edgeCDGasUsed = edgeCD.receipt.gasUsed;
     const edgeCE = await sc.insertAttack(2, 4, '');
+    const edgeCEGasUsed = edgeCE.receipt.gasUsed;
     const edgeDB = await sc.insertAttack(3, 1, '');
+    const edgeDBGasUsed = edgeDB.receipt.gasUsed;
     const edgeED = await sc.insertAttack(4, 3, '');
+    const edgeEDGasUsed = edgeED.receipt.gasUsed;
+    console.log(
+      'insertAttack(): ',
+      (edgeBCGasUsed +
+        edgeCDGasUsed +
+        edgeCEGasUsed +
+        edgeDBGasUsed +
+        edgeEDGasUsed) /
+        5
+    );
 
     const g = await sc.getGraph(1);
-    //printGraph(g);
+    printGraph(g);
 
-    const r4 = await sc.enumeratingPreferredExtensions(1);
-    /* r4.logs.forEach((element) => {
+    const resReduction3 = await sc.pafReductionToAfPr3();
+    const r3 = await sc.getGraph(3);
+    printGraph(r3);
+    const resReduction3GasUsed = resReduction3.receipt.gasUsed;
+    console.log('pafReductionToAfPr3(): ', resReduction3GasUsed);
+
+    const r4 = await sc.enumeratingPreferredExtensions(2);
+    r4.logs.forEach((element) => {
       console.log('*************************************');
       console.log(element.args.args);
-    });*/
-/*  });
+    });
+    const r4GasUsed = r4.receipt.gasUsed;
+    console.log('enumeratingPreferredExtensions(): ', r4GasUsed);
+  });
 });
-for (let i = 0; i < 20; i++) {
+
+/*
+for (let i = 0; i < 1; i++) {
   contract('Argumentation N', (accounts) => {
     const alpha = accounts[0];
     const beta = accounts[1];
     const gamma = accounts[2];
     const prefP = 0.25;
-    const nodesNumber = 20;
-    const edgesP = 0.5;
+    const nodesNumber = 5;
+    const edgesP = 0.66;
     let edgesNumber = 0;
 
     it('random graphs', async () => {
@@ -140,10 +201,10 @@ for (let i = 0; i < 20; i++) {
       console.log(reductionGasUsed);
 
       const r4 = await sc.enumeratingPreferredExtensions(2);
-      /*r4.logs.forEach((element) => {
-        console.log('*************************************');
-        console.log(element.args.args);
-      });
+      //r4.logs.forEach((element) => {
+      //  console.log('*************************************');
+      //  console.log(element.args.args);
+      //});
       const gasUsed = r4.receipt.gasUsed;
       console.log(gasUsed);
 
@@ -155,28 +216,9 @@ for (let i = 0; i < 20; i++) {
     });
   });
 }
-
 */
-const printGraph = (g) => {
-  console.log('--------Graph--------');
-  for (const node of g.nodes) {
-    console.log('Node:', node.toString());
-  }
 
-  for (let i = 0; i < g.edgesSource.length; i++) {
-    console.log(
-      g.edgesSource[i].toString(),
-      ' -> ',
-      g.edgesTarget[i].toString()
-    );
-  }
-};
-
-const getRandomIntInclusive = (min, max) => {
-  min = Math.ceil(min);
-  max = Math.floor(max);
-  return Math.floor(Math.random() * (max - min + 1) + min); //The maximum is inclusive and the minimum is inclusive
-};
+////////////////////////////// NEGOTIATION
 
 const scoringFunction = (x, constj) => {
   if (constj.Vjdec) {
@@ -244,13 +286,15 @@ contract('Negotiation', (accounts) => {
     const res1 = await sc.newNegotiation(1, alpha, beta, {
       from: alpha,
     });
-    console.log('newNegotiation:', res1.receipt.gasUsed);
+    console.log('newNegotiation(): ', res1.receipt.gasUsed);
 
+    // negotiation thread
+    const proposalsGasUsage = [];
     let xba = timeDependentTactic(negotiationPoly(0, constjBeta), constjBeta);
     const res2 = await sc.newProposal(0, Math.floor(xba * 10000), {
       from: beta,
     });
-    console.log('newProposal:', res2.receipt.gasUsed);
+    proposalsGasUsage.push(res2.receipt.gasUsed);
     let xab;
     for (let t = 1; t <= tmax + 1; t++) {
       if (t % 2) {
@@ -261,14 +305,14 @@ contract('Negotiation', (accounts) => {
             const resFinalA = await sc.accept(0, Math.floor(xba * 10000), {
               from: alpha,
             });
-            console.log('accept:', resFinalA.receipt.gasUsed);
+            console.log('accept(): ', resFinalA.receipt.gasUsed);
           }
           break;
         } else xab = res;
         const res3 = await sc.newProposal(0, Math.floor(xab * 10000), {
           from: alpha,
         });
-        console.log('newProposal:', res3.receipt.gasUsed);
+        proposalsGasUsage.push(res3.receipt.gasUsed);
       } else {
         const res = interpretation(t, xab, constjBeta);
         console.log(t, ') - b - ', res);
@@ -277,15 +321,20 @@ contract('Negotiation', (accounts) => {
             const resFinalB = await sc.accept(0, Math.floor(xab * 10000), {
               from: beta,
             });
-            console.log('accept:', resFinalB.receipt.gasUsed);
+            console.log('accept(): ', resFinalB.receipt.gasUsed);
           }
           break;
         } else xba = res;
         const res4 = await sc.newProposal(0, Math.floor(xba * 10000), {
           from: beta,
         });
-        console.log('newProposal:', res4.receipt.gasUsed);
+        proposalsGasUsage.push(res4.receipt.gasUsed);
       }
     }
+    let gasAvgProp = 0;
+    for (const propG of proposalsGasUsage) {
+      gasAvgProp += propG;
+    }
+    console.log('newProposal(): ', gasAvgProp / proposalsGasUsage.length);
   });
 });
