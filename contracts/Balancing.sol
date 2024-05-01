45872d42daab42c25751dad89fb0eeb3361346e1
Quibus <Vincent.j.wit@gmail.com>
2024-05-01 22:13:30 +0200

    fine tuning for paper

--- contracts/Balancing.sol
+++ contracts/Balancing.sol
@@ -35,9 +35,9 @@ contract Balancing {
     constructor() public {
         // for now constructor sets issue to 0 // this is a bit silly as there will only be one issue in the contract but there can be reasons that are relevant for another issue.
         
-        issueTBD = 1;
+        issueTBD = 0;
         
-        changeWeight(1,1);
+        //changeWeight(1,1);
 
         uint256 reasonID = reasonsIds.count() + 1;
         reasonsIds.insert(bytes32(reasonID));
@@ -45,9 +45,12 @@ contract Balancing {
         reason.justification = 0;
         reason.issue = 0;
         reason.polarity = 0;
+        weights[reasonID-1] = 42;
     }
 
 // create functon to initialize contract. create access right for admin to set issue and read rights of the discourse.
+
+
 
     function returnWeight(uint256 r) 
         public
@@ -85,9 +88,18 @@ contract Balancing {
     function getIssue()
         public
         view
-        returns ( uint256 issue)
+        returns ( uint256 )
     {
-        issue = issueTBD;
+        return issueTBD;
+    }
+    
+    function setIssue(uint256 issue)
+    	public
+    {
+    	// TODO change to depend on access rights and can only be called at the start
+    	if(reasonsIds.count() < 2) {
+    	    issueTBD = issue;
+    	}
     }
 
     function getReasons()
@@ -115,19 +127,20 @@ contract Balancing {
         }
     }
 
-    function insertReason(uint256 justification, uint256 issue, uint256 polarity) 
+    function voteOnReason(uint256 justification, uint256 issue, uint256 polarity) 
         public
         returns (int256 re)
     {
 
         re = 0;
         // check that reason is not known yet.
-        for (uint256 j = 0; j < reasonsIds.count() + 1; j++) {
+        for (uint256 j = 1; j < reasonsIds.count() + 1; j++) {
             if (reasons[j].justification == justification && 
             reasons[j].issue == issue && reasons[j].polarity == polarity) {
                 // conclude reason is already in reasons
-                // return false;
-                re = -1;
+                // increase weight by +1
+                weights[j-1]++;
+                re = 1;
                 break;
             }
         }
@@ -139,6 +152,7 @@ contract Balancing {
             reason.justification = justification;
             reason.issue = issue;
             reason.polarity = polarity;
+            weights[reasonID-1]++;
     
             HitchensUnorderedKeySetLib.Set storage source = sources[
                 msg.sender
@@ -146,8 +160,8 @@ contract Balancing {
             source.insert(bytes32(reasonID));
             re = int256(reasonID);
         }
-
     }
+   
 
     function procedureAdditive()
         public
